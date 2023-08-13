e3a34853cb3c9c47e229049bc80e8f4568b8f742
Lucas Alves <Lucasasdev@gmail.com>
2023-08-13 16:35:20 -0300

    Primeira parte do projeto

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const admin = require('./routes/admin.js')
+const path = require('path')
+//const mongoose = require ('mongoose')
+
+//configurações
+  //body-parser
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+    //handlebars
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    //mongoose
+    //public
+    app.use(express.static(path.join(__dirname, 'public')))
+
+//rotas
+    app.get('/', (req, res)=>{
+        res.send('Rota Principal')
+    })
+    app.use('/admin', admin)
+//outros
+
+const PORT = 8081
+app.listen(PORT, ()=>{
+    console.log(`Connected in url http://localhost ${PORT}`)
+})
