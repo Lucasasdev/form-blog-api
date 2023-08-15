9235ae30a52340f6af3ad13ef83018424942cc7f
Lucas Alves <Lucasasdev@gmail.com>
2023-08-15 19:41:40 -0300

    Segunda alteração, evinando dados do formulario para a rota categorias, ainda precisa de ajustes

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin.js')
 const path = require('path')
-//const mongoose = require ('mongoose')
+const mongoose = require ('mongoose')
 
 //configurações
   //body-parser
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: true}))
+
     //handlebars
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+
     //mongoose
+    mongoose.Promise =  global.Promise;
+    mongoose.connect('mongodb://localhost/blogapp', {
+        useNewUrlParser: true
+        //useUnifiedTopology: true
+    }).then(() => {
+        console.log('Mongodb connected!')
+        }).catch((error)=>{
+            console.log(`conection error: ${error}!`)
+    })
+    
     //public
     app.use(express.static(path.join(__dirname, 'public')))
 
